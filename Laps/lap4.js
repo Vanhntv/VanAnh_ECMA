fe36c49c70873561ce80609b06d0e935e179fbfc
Vanhntv <ngothivananh772006@gmail.com>
2025-11-10 13:48:31 +0700

    lap04.js

---
+++ Laps/lap4.js
@@ -0,0 +1,26 @@
+//? Bài 1: Viết hàm trả về phần tử đầu tiên và cuối cùng của mảng
+function getFirstLast(array) {
+  const first = array[0];
+  const second = array[array.length - 1];
+  return [first, second];
+}
+console.log(getFirstLast([1, 2, 3, 4])); // [1, 4]
+
+//? Bài 2: Hoán đổi vị trí phần tử thứ 2 và thứ 4 trong mảng
+function swapElements(arr) {
+  let temp = arr[1];
+  arr[1] = arr[3];
+  arr[3] = temp;
+  return arr;
+}
+
+console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
+
+//? Bài 1: Viết hàm delay trả về Promise
+function delay(ms) {
+  return new Promise((resolver) => {
+    setTimeout(resolver, ms);
+  });
+}
+
+delay(2000).then(() => console.log("2 seconds passed"));

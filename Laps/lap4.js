4785a095b89a94a9af4045e2d88dbb4b533575c9
Vanhntv <ngothivananh772006@gmail.com>
2025-11-12 13:36:10 +0700

    vite

--- Laps/lap4.js
+++ Laps/lap4.js
@@ -18,9 +18,50 @@ console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
 
 //? Bài 1: Viết hàm delay trả về Promise
 function delay(ms) {
-  return new Promise((resolver) => {
+  return new Promise(resolver => {
     setTimeout(resolver, ms);
   });
 }
 
-delay(2000).then(() => console.log("2 seconds passed"));
+delay(2000).then(() => console.log('2 seconds passed'));
+
+//? Bài tập Async/Await
+//* Bài 1: Viết lại callback hell thành async/await
+
+// Viết lại hàm này sử dụng async/await
+// function processOrder(orderId, callback) {
+//   getOrder(orderId, (order) => {
+//     getUser(order.userId, (user) => {
+//       getProducts(order.productIds, (products) => {
+//         callback({ order, user, products });
+//       });
+//     });
+//   });
+// }
+async function processOrder(orderId) {
+  try {
+    const order = await getOrder(orderId);
+    const user = await getUser(order.userId);
+    const products = await getProducts(order.productIds);
+    return { order, user, products };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//? Bài 2: Xử lý lỗi với async/await
+
+async function safeApiCall(apiFunction, ...args) {
+  try {
+    const result = await apiFunction(...args);
+    return {
+      key: true,
+      data: result,
+    };
+  } catch (error) {
+    return {
+      key: false,
+      error: error.message,
+    };
+  }
+}
